b9a4058f00d6cd1ef660908bf368442aec6e7332
adailtonygor@hotmail.com <Nova@NOVARJLAP-029>
2023-04-04 16:02:25 -0300

    feat: Pasta Cadast Add validaçoes de form,Axxios.

--- src/components/Cadastro/Cadastro.jsx
+++ src/components/Cadastro/Cadastro.jsx
@@ -1,121 +1,82 @@
-import React, { useState } from 'react';
-import { TextField, Button, Grid, Box } from '@material-ui/core';
-import {Link} from 'react-router-dom';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { TextField, Button, RadioGroup, Radio, FormControlLabel,Grid } from '@material-ui/core';
+import Axios from 'axios';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
+const schema = yup.object().shape({
+  name: yup.string().required('Nome e obrigatorio'),
+  cpf: yup.string().required('CPF e obrigatorio').matches(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/, 'CPF must be in the format XXX.XXX.XXX-XX'),
+  address: yup.string().required('Endereço obrigatorio'),
+  birthday: yup.date().required('Birthday is required'),
+  sex: yup.string().required('Sex is required')
+});
 
+const Cadastro = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
 
-function Cadastro() {
-  const [nome, setNome] = useState('');
-  const [cpf, setCpf] = useState('');
-  const [endereco, setEndereco] = useState('');
-  const [complemento, setComplemento] = useState('');
-  const [dataNascimento, setDataNascimento] = useState('');
-
-  const handleNomeChange = (event) => {
-    const value = event.target.value.replace(/[^a-zA-Z\s]/g, '').substr(0, 50);
-    setNome(value);
-  };
-
-  const handleCpfChange = (event) => {
-    const value = event.target.value.replace(/[^0-9]/g, '').substr(0, 11);
-    setCpf(value);
-  };
-
-  const handleEnderecoChange = (event) => {
-    const value = event.target.value.substr(0, 50);
-    setEndereco(value);
-  };
-
-  const handleComplementoChange = (event) => {
-    const value = event.target.value.substr(0, 50);
-    setComplemento(value);
-  };
-
-  const handleDataNascimentoChange = (event) => {
-    setDataNascimento(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log({
-      nome,
-      cpf,
-      endereco,
-      complemento,
-      dataNascimento,
-    });
-    <Link to="/">Voltar para Home</Link>
-  };
+  const onSubmit = async (data) => {
+    try {
+      const response = await Axios.post('/api/register', data);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Nome"
-            value={nome}
-            onChange={handleNomeChange}
-            inputProps={{ maxLength: 50 }}
-           
-          />
-
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="CPF"
-            value={cpf}
-            onChange={handleCpfChange}
-            inputProps={{ maxLength: 11 }}
-          />
-
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Endereço"
-            value={endereco}
-            onChange={handleEnderecoChange}
-            inputProps={{ maxLength: 50 }}
-          />
-
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Complemento"
-            value={complemento}
-            onChange={handleComplementoChange}
-            inputProps={{ maxLength: 50 }}
-            
-          />
-
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Data de Nascimento"
-            type="date"
-            value={dataNascimento}
-            onChange={handleDataNascimentoChange}
-            InputLabelProps={{
-              shrink: true,
-              
-            }}
-          />
-
-        </Grid>
-        <Grid item xs={12}>
-          <Box textAlign ="center">
-          <Button type="submit" variant="contained" color="primary">
-            Cadastrar
-          </Button>
-          </Box>
-        </Grid>
+    <form onSubmit={handleSubmit(onSubmit)}>
+       <Grid container direction="column"  alignItems="center" spacing={3}>
+        <Grid item >
+      <TextField
+      
+        label="Name"
+        {...register("name")}
+        error={!!errors.name}
+        helperText={errors.name?.message}
+      />
+      </Grid>
+      <Grid item >
+      <TextField
+        label="CPF"
+        {...register("cpf")}
+        error={!!errors.cpf}
+        helperText={errors.cpf?.message}
+      />
+      </Grid>
+      <Grid item >
+      <TextField
+        label="Address"
+        {...register("address")}
+        error={!!errors.address}
+        helperText={errors.address?.message}
+      />
+       </Grid>
+       <Grid item >
+      <TextField
+        label="Birthday"
+        type="date"
+        {...register("birthday")}
+        error={!!errors.birthday}
+        helperText={errors.birthday?.message}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+      </Grid>
+      <Grid item >
+      <RadioGroup {...register("sex")} error={!!errors.sex} helperText={errors.sex?.message}>
+        <FormControlLabel value="Masculino" control={<Radio />} label="Masculino" />
+        <FormControlLabel value="Feminino" control={<Radio />} label="Feminino" />
+      </RadioGroup>
+      <Button type="submit" variant="contained" color="primary">Enviar</Button>
+      </Grid>
       </Grid>
     </form>
   );
-}
+};
 
 export default Cadastro;
